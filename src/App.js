66f7446832096ac55a3924652626d1e064470fbb
INTLGNTdev <quasibodocrew@gmail.com>
2025-05-11 20:44:32 -0600

    Initial commit – full SOL Cart site

---
+++ src/App.js
@@ -0,0 +1,19 @@
+// src/App.js
+import React from 'react';
+import './index.css'; // tailwind directives + any global styles
+
+function App() {
+  return (
+    <div className="min-h-screen bg-gray-900 text-white font-orbitron">
+      {/* TODO: replace this stub with your full SOL Cart site JSX */}
+      <header className="p-8 text-center">
+        <h1 className="text-5xl neon-text">SOL Cart</h1>
+      </header>
+      <main className="p-8">
+        <p>Your site code goes here…</p>
+      </main>
+    </div>
+  );
+}
+
+export default App;
